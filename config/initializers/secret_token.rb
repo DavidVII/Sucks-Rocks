b6d3319b57a9c5d14f79b949098274a6f146b325
David Becerra <davidbecerra84@gmail.com>
2013-08-26 21:26:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SucksRocks::Application.config.secret_key_base = 'bf243c834670ef73ee4555d276aff7edaa066d36771f44a198d5831cb2382fc1fb8821e1414c63eeb25cdf37ba87086e445d6cf6f3886cecf7df8d1698729cd1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SucksRocks::Application.config.session_store :cookie_store, key: '_sucks-rocks_session'
